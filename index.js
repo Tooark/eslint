f23adab6a41e53e2e3d0a83796d1b05ed291e497
Paulo Freitas <paulo.freitas@grupojacto.com.br>
2024-11-03 17:40:56 -0300

    Revisado codigo para .mjs e utilizando estrutura de modulo

--- index.js
+++
@@ -1,8 +0,0 @@
-// Importa a configuração ESLint para projetos React
-import reactConfig from './react.js';
-
-// Importa a configuração ESLint para projetos Node.js
-import nodeConfig from './node.js';
-
-// Exporta as configurações para serem usadas em outros projetos
-export default { reactConfig, nodeConfig };
